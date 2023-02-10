b9b44d03d35c99a93bc86b887cd514979ae1e42d
Nahomy Castro <ncastrom21@gmail.com>
2023-02-10 11:35:50 -0600

    Intento de conflicto en GIT

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,6 @@
 public class UI {
-    
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al centro de adopcioanes de animales");
+        System.out.println("¿Qué tipo de animal quieres adoptar?");
+    }
 }
