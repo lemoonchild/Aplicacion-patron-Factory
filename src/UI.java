b7466393064c673ab3be87f6f78545d341006c91
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-02-10 11:35:53 -0600

    Segundo intento de error en GIT

--- src/UI.java
+++ src/UI.java
@@ -3,5 +3,9 @@ public class UI {
     
     public static void main(String[] args) {
         System.out.println("Centro de adopciones de animales");
+        System.out.println("Opciones de adopación");
+        System.out.println("\n 1.) ");
+        System.out.println("\n 2.) ");
+        System.out.println("\n 3.) ");
     }
 }
